8038d083b0777c75652a21bf50ca09be02842f9c
chadongju <jason.c9340@gmail.com>
2023-09-21 04:16:13 +0900

    젯팩 공부 다시시작

---
+++ app/src/main/java/com/example/jetpackapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.jetpackapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn1 : Button = findViewById(R.id.btn1)
+        btn1.text = "abc"
+    }
+}
